2b49325e433e2e53fc6961ea42d1a72c622eac91
Ishaan Pathak <vaxtnhwm@anonaddy.me>
2023-08-24 17:24:49 +0530

    Add rook legal moves function

--- src/main.rs
+++ src/main.rs
@@ -298,14 +298,46 @@ fn get_bishop_moves(board: &[[Option<ChessPiece>; 11]; 11], current_coordinates:
     
     legal_moves
 }
-                    stop = true;
+
+fn get_rook_moves(board: &[[Option<ChessPiece>; 11]; 11], current_coordinates: &(usize, usize)) -> Vec<(usize, usize)> {
+    let mut legal_moves: Vec<(usize, usize)> = Vec::new();
+    let mut stop: bool = false;
+    let mut coordinates = *current_coordinates;
+    let check_directions = [
+        MoveDirection::LeftUp, MoveDirection::Up, MoveDirection::RightUp,
+        MoveDirection::LeftDown, MoveDirection::Down, MoveDirection::RightDown,
+    ];
+    for direction in check_directions.iter() {
+        while !stop {
+            let next_coordinate = get_move_coordinate(*direction, &coordinates);
+            if is_coordinate_in_bounds(&next_coordinate) {
+                if let Some(next_piece) = board[next_coordinate.0][next_coordinate.1] {
+                    if let Some(current_piece) = board[current_coordinates.0][current_coordinates.1] {
+                        if next_piece.color != PieceColor::None { 
+                            if next_piece.color != current_piece.color {
+                                legal_moves.push(next_coordinate);
+                                stop = true;
+                            } else {
+                                stop = true;
+                            }
+                        } else {
+                            legal_moves.push(next_coordinate);
+                            coordinates = next_coordinate;
+                        }
+                    }
+                } else {
+                    legal_moves.push(next_coordinate);
+                    coordinates = next_coordinate;
                 }
             } else {
                 stop = true;
             }
         }
         coordinates = *current_coordinates;
+        stop = false;
     }
+    legal_moves
+}
     
     legal_moves
 }
