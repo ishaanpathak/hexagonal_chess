d346887845fdbb11c28aa18c490c02cd62782858
Ishaan Pathak <vaxtnhwm@anonaddy.me>
2023-08-30 13:20:44 +0530

    Add fields to Game struct
    Added new fields to the Game struct for having more information about
    the game. Also added a GameResult enum but haven't used yet.

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,26 @@
-struct Game {
-    id: u32,
-    move_history: Vec<[(usize, usize); 2]>,
-    board: [[Option<ChessPiece>; 11]; 11]
+use crate::pieces::PieceColor;
+use crate::pieces::ChessPiece;
+
+enum GameResult {
+    Ongoing,
+    Checkmate,
+    Stalemate,
+    Draw
+}
+
+/// This struct is responsible for storing the Game's state.
+/// 
+/// # Feilds and their Meanings
+/// 
+/// 1. `id`: This ID will be useful for storing, identifying and loading gamess.
+/// 2. `move_history`: This is a Vector of (x,y) coordinates which will basically tell us which piece was move to which coordinate.
+/// 3. `board`: This is the actual board itself. Contains all the places, and pieces.
+/// 4. `next_turn`: This will store which color will play next.
+/// 5. `captured_pieces`: Array of 2 Vectors, 1 for Black and 1 for White. Each vector will store what Pieces have Black and White sides captured.
+pub struct Game {
+    pub id: u32,
+    pub move_history: Vec<[(usize, usize); 2]>,
+    pub board: [[Option<ChessPiece>; 11]; 11],
+    pub next_turn: PieceColor,
+    pub captured_pieces: [Vec<ChessPiece>; 2]
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod pieces;
 mod moves;
 mod board;
 mod config;
+mod game;
 
 use crate::moves::*;
 use crate::board::*;
 use crate::config::load_config;
 use crate::pieces::ChessPiece;
+use crate::game::Game;
 
 fn main() {
     let board: [[Option<ChessPiece>; 11]; 11];
@@ -26,4 +28,12 @@ fn main() {
     println!("{:?}", get_legal_moves(&board, &(4,5)));
     println!("{:?}", get_legal_moves(&board, &(4,3)));
     println!("{:?}", get_legal_moves(&board, &(3,7)));
+    
+    let _current_game = Game {
+        id: 1,
+        move_history: Vec::new(),
+        board: board.clone(),
+        next_turn: pieces::PieceColor::White,
+        captured_pieces: [Vec::new(), Vec::new()],
+    };
 }
