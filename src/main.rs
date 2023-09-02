13e3f803bcdc3683cdfda767363854a7170734a9
Ishaan Pathak <vaxtnhwm@anonaddy.me>
2023-09-03 02:07:59 +0530

    Testing checkmate, stalemate & check

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ mod moves;
 mod board;
 mod config;
 mod game;
+mod validation;
+
+use pieces::PieceColor;
 
 use crate::moves::*;
 use crate::board::*;
 use crate::config::load_config;
-use crate::pieces::ChessPiece;
 use crate::game::Game;
 
 fn main() {
@@ -23,17 +25,41 @@ fn main() {
 
     print_board(&board);
 
-    println!("{:?}", get_legal_moves(&board, &(6,5)));
-    println!("{:?}", get_legal_moves(&board, &(6,8)));
-    println!("{:?}", get_legal_moves(&board, &(4,5)));
-    println!("{:?}", get_legal_moves(&board, &(4,3)));
-    println!("{:?}", get_legal_moves(&board, &(3,7)));
-    
     let _current_game = Game {
         id: 1,
         move_history: Vec::new(),
         board: board.clone(),
         next_turn: pieces::PieceColor::White,
         captured_pieces: [Vec::new(), Vec::new()],
+        result: game::GameResult::Ongoing
     };
+
+    let chance = PieceColor::White;
+    
+    let current_pieces = pieces::get_all_pieces(&board, chance);
+
+    let mut all_moves: MoveList = MoveList::new();
+    
+    for piece in current_pieces {
+        let current_piece_moves = get_legal_moves(&board, &(piece.x, piece.y));
+        all_moves.insert((piece.x, piece.y), current_piece_moves);
+    }
+    
+    let in_check = validation::is_check(&board, chance);
+    
+    remove_check_moves(&board, &mut all_moves);
+    
+    if in_check {
+        if validation::has_legal_moves(all_moves) {
+            println!("Check!");
+        } else {
+            println!("Checkmate!");
+        }
+    } else {
+        if !validation::has_legal_moves(all_moves) {
+            println!("Stalemate!");
+        } else {
+            println!("Nothing");
+        }
+    }
 }
