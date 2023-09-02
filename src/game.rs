73035f82e5d2e206c1a1e4fd9f662ea750da4974
Ishaan Pathak <vaxtnhwm@anonaddy.me>
2023-09-03 01:56:27 +0530

    Make GameResult public, add new fields to Game

--- src/game.rs
+++ src/game.rs
@@ -2,7 +2,8 @@ use crate::pieces::PieceColor;
 use crate::pieces::ChessPiece;
 use crate::board::Board;
 
-enum GameResult {
+/// Struct that contains what the Game's current Result is. Can be Ongoing, Checkmate, Stalemate, Draw.
+pub enum GameResult {
     Ongoing,
     Checkmate,
     Stalemate,
@@ -11,17 +12,19 @@ enum GameResult {
 
 /// This struct is responsible for storing the Game's state.
 /// 
-/// # Feilds and their Meanings
+/// # Fields and their Meanings
 /// 
-/// 1. `id`: This ID will be useful for storing, identifying and loading gamess.
+/// 1. `id`: This ID will be useful for storing, identifying and loading games.
 /// 2. `move_history`: This is a Vector of (x,y) coordinates which will basically tell us which piece was move to which coordinate.
 /// 3. `board`: This is the actual board itself. Contains all the places, and pieces.
 /// 4. `next_turn`: This will store which color will play next.
 /// 5. `captured_pieces`: Array of 2 Vectors, 1 for Black and 1 for White. Each vector will store what Pieces have Black and White sides captured.
+/// 6. `result`: Contains state of the Game: Ongoing, Checkmate, Stalemate, Draw
 pub struct Game {
     pub id: u32,
     pub move_history: Vec<[(usize, usize); 2]>,
     pub board: Board,
     pub next_turn: PieceColor,
-    pub captured_pieces: [Vec<ChessPiece>; 2]
+    pub captured_pieces: [Vec<ChessPiece>; 2],
+    pub result: GameResult
 }
