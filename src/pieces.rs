64c147627e7bf7ce5f3ae351e48d6be89f95f7ec
Ishaan Pathak <vaxtnhwm@anonaddy.me>
2023-09-03 01:57:45 +0530

    Add function to get all pieces of a PieceColor
    This function will be used in other functions. Lot of places where I want to get all the pieces of a particular color.

--- src/pieces.rs
+++ src/pieces.rs
@@ -1,3 +1,5 @@
+use crate::board::Board;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum PieceType {
     Pawn,
@@ -20,4 +22,27 @@ pub enum PieceColor {
 pub struct ChessPiece {
     pub piece_type: PieceType,
     pub color: PieceColor,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Piece {
+    pub x: usize,
+    pub y: usize,
+    pub piece: ChessPiece
+}
+
+pub fn get_all_pieces(board: &Board, check_color: PieceColor) -> Vec<Piece> {
+    let mut all_pieces: Vec<Piece> = Vec::new();
+    for i in 0usize..11usize {
+        for j in 0usize..11usize {
+            if (i + j > 4) && (i + j < 16) {
+                if let Some(current_piece) = board[i][j] {
+                    if current_piece.color == check_color {
+                        all_pieces.push(Piece { x: i, y: j, piece: current_piece });
+                    }
+                }
+            }
+        }
+    }
+    all_pieces
 }
