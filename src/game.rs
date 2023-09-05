bb2a09f396cfced45f914f0def4634be218f1ad9
Ishaan Pathak <vaxtnhwm@anonaddy.me>
2023-09-05 18:00:43 +0530

    Add History type for storing move history

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,12 @@
 use crate::pieces::PieceColor;
 use crate::pieces::ChessPiece;
 use crate::board::Board;
+use crate::moves::Move;
+
+type History = Vec<Move>;
 
 /// Struct that contains what the Game's current Result is. Can be Ongoing, Checkmate, Stalemate, Draw.
+#[derive(PartialEq, Debug)]
 pub enum GameResult {
     Ongoing,
     Checkmate,
@@ -15,14 +19,14 @@ pub enum GameResult {
 /// # Fields and their Meanings
 /// 
 /// 1. `id`: This ID will be useful for storing, identifying and loading games.
-/// 2. `move_history`: This is a Vector of (x,y) coordinates which will basically tell us which piece was move to which coordinate.
+/// 2. `move_history`: Vector of Moves. This will store all the moves that have been played in the game.
 /// 3. `board`: This is the actual board itself. Contains all the places, and pieces.
 /// 4. `next_turn`: This will store which color will play next.
 /// 5. `captured_pieces`: Array of 2 Vectors, 1 for Black and 1 for White. Each vector will store what Pieces have Black and White sides captured.
 /// 6. `result`: Contains state of the Game: Ongoing, Checkmate, Stalemate, Draw
 pub struct Game {
     pub id: u32,
-    pub move_history: Vec<[(usize, usize); 2]>,
+    pub move_history: History,
     pub board: Board,
     pub next_turn: PieceColor,
     pub captured_pieces: [Vec<ChessPiece>; 2],
